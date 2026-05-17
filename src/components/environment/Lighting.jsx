36c88a0f8c3fc302ec13125c84217e3e19f57692
kumaran <k31311558@gmail.com>
2026-05-17 14:43:52 +0530

    Adjust lighting settings in Lighting component

--- src/components/environment/Lighting.jsx
+++ src/components/environment/Lighting.jsx
@@ -1,17 +1,41 @@
 export default function Lighting() {
+
   return (
     <>
-      <ambientLight intensity={0.4} />
+
+      {/* Ambient */}
+      <ambientLight intensity={0.2} />
+
+      {/* Moon/Sun */}
       <directionalLight
-        position={[5, 10, 5]}
+        position={[10, 20, 10]}
         intensity={2}
         castShadow
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
       />
+
+      {/* Blue neon */}
       <pointLight
         position={[0, 5, 0]}
         color="#00ffff"
+        intensity={15}
+      />
+
+      {/* Purple glow */}
+      <pointLight
+        position={[10, 3, 10]}
+        color="#aa00ff"
         intensity={10}
       />
+
+      {/* Red edge light */}
+      <pointLight
+        position={[-10, 3, -10]}
+        color="#ff0033"
+        intensity={8}
+      />
+
     </>
   );
 }
